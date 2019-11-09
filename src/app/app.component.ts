1f5edcbef7f890c35b726a7b861427b270f59c73
juan <juanma_gsm@hotmail.com>
2019-11-09 14:22:24 -0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Integrador';
+  items: Observable<any[]>
+  constructor(db: AngularFirestore){
+    this.items = db.collection('empleados').valueChanges();
+  }
+
+
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBBpdWbedndiSYJzcJbqltVXm7_aSRHGEQ",
+    authDomain: "integrador-e1e80.firebaseapp.com",
+    databaseURL: "https://integrador-e1e80.firebaseio.com",
+    projectId: "integrador-e1e80",
+    storageBucket: "integrador-e1e80.appspot.com",
+    messagingSenderId: "449291565619",
+    appId: "1:449291565619:web:04858f74592dc91ec3c146",
+    measurementId: "G-Z6VZ99F45J"
+  }
 };
