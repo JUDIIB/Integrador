4ba85ef99d4fd4afa3cb6242293bc0065a428e98
Augusto Romero <geragusto@hotmail.com>
2019-12-01 20:28:39 -0300

    Trigger Function deleteAllRecordsFromEmployee
    Cloud Function Deployed To Firebase for Deleting records related to Employee

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,27 @@
 import * as functions from 'firebase-functions';
-
+import * as admin from 'firebase-admin';
+admin.initializeApp();
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+/* export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
-});
+}); */
+
+//Se ejecuta cuando se elimina el registro de un empleado
+exports.deleteAllRecordsFromEmployee = functions.firestore
+    .document(`empleados/{empleadoId}`)
+    .onDelete(async (snapshot) => {
+        const empleadoId = snapshot.id;
+        //Se eliminan las vacaciones asociadas al empleado
+        await admin
+            .firestore().collection('vacaciones').where('empleado_id', '==', empleadoId).get()
+            .then(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
+                    admin.firestore().collection('vacaciones').doc(doc.id).delete();
+                });
+            })
+            .catch(function (error) {
+                console.log("Error getting documents: ", error);
+            });
+    })
